fb67d23d22fc4828ca93cc770a2abf2a29ac88ac
10007327 <31414889+10007327@users.noreply.github.com>
2018-05-03 10:45:48 -0600

    Update Finalproject(2).cpp

--- Finalproject(2).cpp
+++ Finalproject(2).cpp
@@ -1,1 +1,30 @@
+string productName = "";
+	string productAvailbe = "";
+	
+	
+	cout << "enter name of the product";
+	cin >> productName;
+	cout << "enter the amount wanted of the product";
+	cin >> productAvailbe;
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
