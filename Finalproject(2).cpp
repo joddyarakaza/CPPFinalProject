553e76fd72151962f95e935ad56c78daeb5e8b37
10007327 <31414889+10007327@users.noreply.github.com>
2018-05-09 11:55:37 -0600

    Update Finalproject(2).cpp

--- Finalproject(2).cpp
+++ Finalproject(2).cpp
@@ -1,62 +1,52 @@
 
-
 #include <iostream>
+#include <fstream>
 #include <string>
 
-
 using namespace std;
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]){
+	
+		string productName = "";
+		string productAvailbe = "";
 		
 		
-		int main ()
-		{
+		cout << "enter name of the product(enter -1 to quit) :";
+		cin >> productName;
+		cout << "enter the amount wanted of the product :";
+		cin >> productAvailbe;
+		
+		ifstream inFile;
+		
+		inFile .open("list.txt" , ios:: in);
+		
+		while (productName != "-1")
+			if (inFile.is_open())
+			
+				
+				while (!inFile.eof()){
+					string prodName = "";
+					string prodAvailbe = "";
+					getline(inFile, prodName, '#');
+					getline(inFile, prodAvailbe);
+					if (prodName == productName ){ 
+						cout << prodName << endl;
+						if (prodAvailbe == productAvailbe){
+							cout << prodAvailbe << endl; 
+							}
+						}
+				cout << "enter name of the product(enter -1 to quit) :";
+				cin >> productName;
+				cout << "enter the amount wanted of the product :";
+				cin >> productAvailbe;		
+				}
+				
+				
+				//close the file 
+				inFile.close();
+				}
 		
 		
-	string productName = "";
-	int productAvailbe = "";
-	
-	string proName = ""; 
-	int proAvailbe = 0;
-	
-	
-	
-	cout << "enter name of the product";
-	cin >> productName;
-	cout << "enter the amount wanted of the product";
-	cin >> productAvailbe;
-	
-	ifstream inFile;
-	
-	inFile .open("list.txt" , ios:: in);
-	
-	if (inFile.is_open())
-	
-		string prodName = "";
-		int prodAvailbe = 0;
-		while (inFile.eof()){
-		getLine(inFile, prodName, '#');
-		getLine(inFile, prodAvailbe);
-		if (prodName == productName ) & (prodAvailbe == productAvavilbe){
-			prodName = proName)   
-		}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+				
+			
 
