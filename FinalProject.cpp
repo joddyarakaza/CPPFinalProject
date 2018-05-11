1c09c5bd7b93bc75bdc3bfc5ab24f55d5d291c74
joddyarakaza <31076841+joddyarakaza@users.noreply.github.com>
2018-05-11 10:36:41 -0600

    Update FinalProject.cpp

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -1,35 +1,66 @@
 
 
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <string>
+#include <sstream>
 
 using namespace std;
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]){
 	
-	ofstream outfile;
-	int itemNum = 0;
-	string item = "";
-	
-	
-	outfile.open("list.txt");
-	
-	if(outfile.is_open()){
-		cout << "Enter the item name(enter -1 to quit): ";
-		cin >> item;
-		while (item != "-1"){
-			cout << "Enter how much of the item there is: ";
-			cin >> itemNum;
-			outfile << item << '#' << itemNum << endl;
-			cout << "Enter the item name(enter -1 to quit): ";
-			cin >> item;
-												
+		string productName = "";
+		int productAvailbe = 0;
+		bool isFound = false;
+		
+		
+		cout << "enter name of the product(enter -1 to quit): ";
+		cin >> productName;
+		cout << "enter the amount wanted of the product: ";
+		cin >> productAvailbe;
+		
+		ifstream inFile;
+		
+		
+		
+		while (productName != "-1"){
+			inFile.open("list.txt");
+			
+			if (inFile.is_open()){
+								
+				while (!inFile.eof()){
+					string prodName = "";
+					string prodAvailbe = "";
+					
+					getline(inFile, prodName, '#');
+					getline(inFile, prodAvailbe);
+					stringstream geek(prodAvailbe);
+					int testVar = 0;
+					geek >> testVar;
+					cout << testVar << endl;
+					if (prodName == productName){
+						isFound = true;
+						if (testVar >= productAvailbe){
+							cout << prodName << " are available in this amount: " << prodAvailbe << endl;
+						}
+						else{
+							cout << "There isn't enough of the product available" << endl;
+						}
+					}
+					
+					cout << "enter name of the product(enter -1 to quit) :";
+					cin >> productName;
+					cout << "enter the amount wanted of the product :";
+					cin >> productAvailbe;		
+				}								
+				//close the file 
+				inFile.close();
+				if(isFound == false){
+					cout << "The product could not be found" << endl;
+				}
+			}
+			else{
+				cout << "The file could not be opened" << endl;
+				productName = -1;
+			}
 		}
-		outfile.close();	
-	}
-	else{
-		cout << "The file could not be opened" << endl;
-	}
-	
 }
-
