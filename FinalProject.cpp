2795a16dd92ef6ca4174e3e8e968c0de71ec991d
joddyarakaza <31076841+joddyarakaza@users.noreply.github.com>
2018-05-11 11:06:22 -0600

    Update FinalProject.cpp

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -33,34 +33,40 @@ int main(int argc, char *argv[]){
 					
 					getline(inFile, prodName, '#');
 					getline(inFile, prodAvailbe);
-					stringstream geek(prodAvailbe);
-					int testVar = 0;
-					geek >> testVar;
-					cout << testVar << endl;
+					
 					if (prodName == productName){
 						isFound = true;
+						stringstream geek(prodAvailbe);
+						int testVar = 0;
+						geek >> testVar;
 						if (testVar >= productAvailbe){
 							cout << prodName << " are available in this amount: " << prodAvailbe << endl;
+							
 						}
 						else{
 							cout << "There isn't enough of the product available" << endl;
 						}
+						string temp = "";
+						inFile >> temp
 					}
-					
-					cout << "enter name of the product(enter -1 to quit) :";
-					cin >> productName;
-					cout << "enter the amount wanted of the product :";
-					cin >> productAvailbe;		
-				}								
-				//close the file 
-				inFile.close();
+							
+				}
 				if(isFound == false){
 					cout << "The product could not be found" << endl;
 				}
+				isFound = false;
+			
 			}
 			else{
 				cout << "The file could not be opened" << endl;
 				productName = -1;
 			}
+			//close the file 
+			inFile.close();
+
+			cout << "enter name of the product(enter -1 to quit) :";
+			cin >> productName;
+			cout << "enter the amount wanted of the product :";
+			cin >> productAvailbe;
 		}
 }
