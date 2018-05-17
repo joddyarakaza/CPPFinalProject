adb7d0b6ed4bc2054285b30be38309555414e00c
joddyarakaza <31076841+joddyarakaza@users.noreply.github.com>
2018-05-17 10:30:05 -0600

    Update FinalProject.cpp

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -1,5 +1,7 @@
 /*
 iStore CustomerEnd
+Created by Joddy Arakaza and Damian Guerero
+Last edit: 05/17/2018
 */
 
 #include <iostream>
@@ -9,36 +11,36 @@ iStore CustomerEnd
 
 using namespace std;
 int main(int argc, char *argv[]){
-	
+
 		string productName = "";
 		int productAvailbe = 0;
 		bool isFound = false;
-		
+
 		ifstream inFile;
 		ofstream outFile;
 
-		
+
 		cout << "*The first letter of the product should be capitlized" << "\n" << "The amount needed should be entered as an integer*" << "\n" << "\n" << endl;
 		cout << "Enter name of the product(enter -1 to quit): ";
 		cin >> productName;
-						
-		
-		
+
+
+
 		while (productName != "-1"){
 			cout << "Enter the amount wanted of the product: ";
 			cin >> productAvailbe;
 
 			inFile.open("list.txt");
-			
+
 			if (inFile.is_open()){
-								
+
 				while (!inFile.eof()){
 					string prodName = "";
 					string prodAvailbe = "";
-					
+
 					getline(inFile, prodName, '#');
 					getline(inFile, prodAvailbe);
-					
+
 					if (prodName == productName){
 						isFound = true;
 						stringstream geek(prodAvailbe);
@@ -47,7 +49,7 @@ int main(int argc, char *argv[]){
 						if (testVar >= productAvailbe){
 							cout << prodName << " are available in this amount: " << prodAvailbe << endl;
 							testVar -= productAvailbe;
-							
+
 						}
 						else{
 							cout << "There isn't enough of the product available" << endl;
@@ -56,7 +58,7 @@ int main(int argc, char *argv[]){
 						inFile >> temp;
 						int prod = temp.find(prodName, 0);
 						int avail = temp.find("#", prod);
-						
+
 						string result;
 						ostringstream convert;
 						convert << testVar;
@@ -66,13 +68,13 @@ int main(int argc, char *argv[]){
 						outFile << temp;
 						*/
 					}
-							
+					 else if(isFound == false){
+						cout << "The product could not be found" << endl;
+					}
+					isFound = false;
+
 				}
-				if(isFound == false){
-					cout << "The product could not be found" << endl;
-				}
-				isFound = false;
-			
+
 			}
 			else{
 				cout << "The file could not be opened" << endl;
